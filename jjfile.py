f6604ed8370b290f4a5f3e7288d79b9ea78eb8c7
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2018-03-05 12:41:27 +0900

    add jjfile

---
+++ jjfile.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+menu = [
+    ('cancel',
+     ""),
+    ('flake8',
+     "flake8 ."),
+    ('upload pypi',
+     "./setup.py sdist; twine upload dist/*"),
+]
